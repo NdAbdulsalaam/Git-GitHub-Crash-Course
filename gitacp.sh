a0aa4ff2107799a19e80d1832c6245e665eba425
Nurudeen Abdulsalaam <olaitansalaam2012@gmail.com>
2025-09-09 21:22:35 +0100

    Accept multiple files as arg

--- gitacp.sh
+++ gitacp.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
 gitacp() {
-    if [ "$#" -eq 0 ]; then
+    # Default commit message
     message="Update changes"
+
+    # If last argument looks like a commit message (in quotes), capture it
+    if [ "$#" -gt 0 ]; then
+        # Last argument is commit message
+        message="${!#}"
+        # All but last argument are files
+        files=("${@:1:$#-1}")
     else
-        message="$1"
+        files=()
     fi
-    git add .
+
+    if [ ${#files[@]} -eq 0 ]; then
+        git add .
+    else
+        git add "${files[@]}"
+    fi
+
     git commit -m "$message" || return
     echo "commit message: $message"
     git push
 }
 
 gitacp "$@"
-
